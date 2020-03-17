3aabf7a08dba03befdda58a6a888630ff3d01159
crystalDf <chendong333@gmail.com>
2020-03-17 21:54:12 +0800

    05 scrabble-score

---
+++ src/main/kotlin/ScrabbleScore.kt
@@ -0,0 +1,15 @@
+object ScrabbleScore {
+
+    private fun scoreLetter(c: Char): Int = when (c.toUpperCase()) {
+        in "AEIOULNRST" -> 1
+        in "DG" -> 2
+        in "BCMP" -> 3
+        in "FHVWY" -> 4
+        in "K" -> 5
+        in "JX" -> 8
+        in "QZ" -> 10
+        else -> throw IllegalArgumentException()
+    }
+
+    fun scoreWord(word: String): Int = word.sumBy { scoreLetter(it) }
+}
